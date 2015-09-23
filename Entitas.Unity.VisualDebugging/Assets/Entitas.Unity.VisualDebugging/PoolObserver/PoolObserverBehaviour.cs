2b723186f41675559daa55a986eaf298eaab4bda
gjroelofs <info@codepoke.net>
2015-09-23 10:51:51 +0200

    Assembly reload fix

--- Entitas.Unity.VisualDebugging/Assets/Entitas.Unity.VisualDebugging/PoolObserver/PoolObserverBehaviour.cs
+++ Entitas.Unity.VisualDebugging/Assets/Entitas.Unity.VisualDebugging/PoolObserver/PoolObserverBehaviour.cs
@@ -15,6 +15,26 @@ namespace Entitas.Unity.VisualDebugging
         void Update() {
             if (_poolObserver != null && _poolObserver.entitiesContainer != null) {
                 _poolObserver.entitiesContainer.name = _poolObserver.ToString();
+
+                foreach (Transform entityObj in _poolObserver.entitiesContainer.transform)
+                {
+                    if (null == entityObj) return;
+
+                    var eBeh = entityObj.GetComponent<EntityBehaviour>();
+
+                    if (eBeh == null || eBeh.entity == null) {
+                        try
+                        {
+                            // We might be destroyed due to Assembly Reload. 
+                            // GameObject isn't null, but Unity overrides the == for the null operator to check for destruction.
+                            if (Application.isEditor)
+                                DestroyImmediate(entityObj.gameObject);
+                            else
+                                Destroy(entityObj.gameObject);
+                        }
+                        catch { }
+                    }
+                }
             }
         }
     }
